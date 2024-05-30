8763ba358b30c00f3c90df41faba61524db96390
SaipakhooHooia <119404940+SaipakhooHooia@users.noreply.github.com>
2024-05-30 17:33:32 +0800

    Delete etc directory

--- etc/crawl.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-import json
-def crawl(url):
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read().decode('utf-8'))
-    
-    return (data)
-print(crawl('data/taipei-attractions.json'))
