a555c8e24b21005ed476be913279363e0a54dead
SaipakHooia <kawamotoiscute@gamil.com>
2024-06-20 10:16:08 +0800

    V 0.5

--- taipei-day-trip/static/attraction.js
+++ taipei-day-trip/static/attraction.js
@@ -107,14 +107,14 @@ async function carouselIndicators(item, bookingImageContainer) {
     }
 
     bookingImageContainer.addEventListener('scroll', updateIndicatorsOnScroll);
-
+    //在這邊做和圖片索引對照的indicator狀態的更新
     function updateIndicators(activeIndex) {
         let indicators = document.querySelectorAll(".carousel-indicator");
         indicators.forEach((indicator, index) => {
             indicator.classList.toggle("active", index === activeIndex);
         });
     }
-
+    //在這邊做indicator和圖片索引的比對
     function updateIndicatorsOnScroll() {
         let scrollLeft = bookingImageContainer.scrollLeft;
         let activeIndex = Math.round(scrollLeft / bookingImagesLength);
@@ -155,3 +155,163 @@ async function initialize() {
 }
 
 initialize();
+
+//-------------------------------Login/Signup section----------------------------------------------------
+let signupResult = document.querySelector(".signup-result");
+let loginResult = document.querySelector(".login-result");
+let loginState = false;
+let logout = document.querySelector(".logout");
+
+function signup(event){
+  event.preventDefault();
+  let signupName = document.querySelector(".signup-name").value;
+  let signupEmail = document.querySelector(".signup-email").value;
+  let signupPwd = document.querySelector(".signup-password").value;
+  fetch('/api/user', {
+      method: 'POST', 
+      headers: {
+          'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify({ "name": signupName, "email": signupEmail, "password": signupPwd })
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+  })
+  .then(data => {
+      if (data.ok) {
+          console.log('Signup success.');
+          signupResult.style.display = "block";
+          signupResult.textContent = 'Signup success.';
+      } 
+      else if (data.error) {
+          console.log('Signup failed.');
+          signupResult.style.display = "block";
+          signupResult.textContent = data.message;
+      }
+  })
+  .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+  });
+}
+
+function login(event){
+  event.preventDefault();
+  let loginEmail = document.querySelector(".login-email").value;
+  let loginPwd = document.querySelector(".login-password").value;
+  fetch('/api/user/auth', {
+      method: 'PUT', 
+      headers: {
+          'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify({ "email": loginEmail, "password": loginPwd })
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+  })
+  .then(data => {
+      if (data.token) {
+          localStorage.setItem('accessToken', data.token);
+          console.log('Login success.');
+          loginResult.style.display = "block";
+          loginResult.textContent = 'Login success.';
+          user_data();
+          window.location.reload();
+          loginState = true;
+          if (loginState) {
+            logout.style.display = "block";
+            loginSignupLink.style.display = "none";
+          }
+      } 
+      else if (data.error) {
+          console.log('Login failed.');
+          loginResult.style.display = "block";
+          loginResult.textContent = data.message;
+          loginState = false;
+      }
+  })
+  .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+  });
+}
+
+//let accessToken = localStorage.getItem('accessToken');
+async function user_data() {
+  let accessToken = localStorage.getItem('accessToken');
+  try {
+    let response = await fetch('/api/user/auth', {
+      method: 'GET', 
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json', 
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    let data = await response.json();
+    loginState = true;
+    console.log(data);
+    if (loginState) {
+      logout.style.display = "block";
+      loginSignupLink.style.display = "none";
+    }
+  } 
+  catch (error) {
+    console.error('There was a problem with your fetch operation:', error);
+    loginState = false;
+  }
+}
+
+//---------------------------Popup Dialog------------------------------------------------------
+let loginSignupLink = document.querySelector(".login-signup");
+let popupDialog = document.querySelector(".popup-dialog");
+loginSignupLink.addEventListener("click", function(){
+  if (popupDialog.style.display === 'block'){
+    popupDialog.style.display = 'none';
+    setTimeout(() => {
+      popupDialog.classList.add("active"); 
+  }, 1000);
+  }
+  else{
+    popupDialog.style.display = 'block';
+  }
+});
+
+let closeIcon = document.querySelector(".close-icon");
+closeIcon.addEventListener("click", function(){
+  popupDialog.classList.remove("active");
+  setTimeout(() => {
+    popupDialog.style.display = 'none';
+}, 30);
+});
+
+let switchToLogin = document.querySelector(".switch-to-login");
+let loginArea = document.querySelector(".login-area");
+let signupArea = document.querySelector(".signup-area");
+switchToLogin.addEventListener("click", function(){
+  signupArea.style.display = "none";
+  loginArea.style.display = "block";
+})
+
+let switchToSignup = document.querySelector(".switch-to-signup");
+switchToSignup.addEventListener("click", function(){
+  signupArea.style.display = "block";
+  loginArea.style.display = "none";
+})
+
+//------------------------重載頁面後獲取用戶訊息-----------------------------------
+document.addEventListener("DOMContentLoaded", function() {
+  user_data();
+});
+
+logout.addEventListener("click", function(){
+  localStorage.removeItem('accessToken');
+  window.location.reload();
+})
